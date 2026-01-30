c5471b994885c2cab0289f34cb57eaac3a923a42
d1r <mr.battery.ua@gmail.com>
2026-01-30 14:53:37 +0200

    feat(icons): add lucid icons

--- gig-mart/src/components/GigsList/GigsList.tsx
+++ gig-mart/src/components/GigsList/GigsList.tsx
@@ -1,6 +1,9 @@
 import { useCallback, useEffect, useState } from "react";
 import { fetchGigs } from "../../backend/api";
 import type { Gig } from "../../backend/types";
+
+import { StarIcon } from "lucide-react";
+
 export const GigsList = () => {
   const [loading, setLoading] = useState<boolean>(true);
   const [gigs, setGigs] = useState<Gig[]>([]);
@@ -31,7 +34,10 @@ export const GigsList = () => {
   return (
     <div className="grid grid-cols-3 gap-4">
       {gigs.map((gig) => (
-        <div key={gig.id} className="bg-gray-500 p-4 rounded-md">
+        <div
+          key={gig.id}
+          className="bg-gray-500 p-4 rounded-md shadow-md flex flex-col gap-2"
+        >
           <div className="flex items-center gap-2 bg-gray-600 p-2 rounded-md">
             <img
               src={gig.author.avatar}
@@ -42,8 +48,11 @@ export const GigsList = () => {
           </div>
           <h3>{gig.title}</h3>
           <img src={gig.image} alt={gig.title} />
-          <p>{gig.rating}</p>
-          <p>{gig.price}</p>
+          <div className="flex items-center gap-2">
+            <p>{gig.rating}</p>
+            <StarIcon className="w-4 h-4 text-yellow-500" />
+          </div>
+          <p>Starting at {gig.price} $</p>
           <p>{gig.category}</p>
         </div>
       ))}
